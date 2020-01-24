984f3e1869c53a30d872493a12d134321fad18c7
PadmaGnanapriya <padmaisuru@gmail.com>
2020-01-24 13:17:07 +0530

    finish Lab 01 assignment.
    Signed-off-by: PadmaGnanapriya <padmaisuru@gmail.com>

--- src/Lab_01/Main.java
+++ src/Lab_01/Main.java
@@ -1,8 +1,35 @@
 package Lab_01;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Welcome to University Management System");
+        System.out.println("Press [1] Register as a student");
+        System.out.println("Press [2] Register as a lecturer");
+        int choice = scanner.nextInt();
+        menu(choice);
+
+
     }
+    public static void menu(int x){
+        switch (x){
+            case 1:
+                Student stu=new Student();
+                stu.registation();
+                break;
+            case 2:
+                Lecturer lec=new Lecturer();
+                lec.registation();
+                break;
+            default:
+                System.out.println("Wrong choise");
+                break;
+        }
+    }
+
 }
+
---
+++ src/Lab_01/Student.java
@@ -0,0 +1,45 @@
+package Lab_01;
+
+
+import jdk.nashorn.internal.ir.LexicalContext;
+import jdk.nashorn.internal.ir.LexicalContextNode;
+
+import java.util.Scanner;
+
+public class Student{
+    String name,subject;
+    int age;
+    int course;
+    Scanner scanner =new Scanner(System.in);
+
+    public static void printResult(String name,int age,String subject){
+        System.out.println("Welcome to University Management System\n\n...");
+        System.out.println("Student registation completed");
+        System.out.println("Name      : "+name);
+        System.out.println("Age       : "+age);
+        System.out.println("Subject   : "+subject);
+    }
+
+    public void registation(){
+        System.out.println("Welcome to University Management System\n\n...");
+        System.out.println("Student Registration.");
+        System.out.println("\nPlease submit following informations,");
+        System.out.println("1.  Name  : ");
+        name=scanner.next();
+        System.out.println("2.  Age   : ");
+        age=scanner.nextInt();
+        System.out.println("3.  Select Course number from following list,");
+        System.out.println("    [1] SENG 11111 - Introdution to Programing");
+        System.out.println("    [2] SENG 11112 - Fundamentals of Engineering");
+        System.out.println("    [3] SENG 11113 - Data Structure and Algorithms");
+        course=scanner.nextInt();
+        if(course==1)
+            subject="SENG 11111 - Introdution to Programing";
+        else if(course==2)
+            subject="SENG 11112 - Fundamentals of Engineering";
+        else if(course==3)
+            subject="SENG 11113 - Data Structure and Algorithms";
+
+        printResult(name,age,subject);
+    }
+}
